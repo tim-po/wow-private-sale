2bf5417fa86a3ef14a1adb8b6e04f0d2af1d86d0
Timophey Podolenchuck <timopheypodolenchuck@Timopheys-MacBook-Pro.local>
2022-05-19 16:00:15 +0300

    mainnet contract

--- src/utils/getAddress.js
+++ src/utils/getAddress.js
@@ -1,21 +1,21 @@
 export const getBUSDAddress = () => {
     // main-net
-    // return "0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56";
+    return "0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56";
     // test-net
-    return "0xe10fecd363459964cecff2e91feb3cdee3aa7e21";
+    // return "0xe10fecd363459964cecff2e91feb3cdee3aa7e21";
 };
 
 export const getMMProAddress = () => {
     // main-net
-    // return "0x6067490d05F3cF2fdFFC0e353b1f5Fd6E5CCDF70";
+    return "0x6067490d05F3cF2fdFFC0e353b1f5Fd6E5CCDF70";
     // test-net
-    return "0xa8892b044ece158cb4869b59f1972fa01aae6d2e"
+    // return "0xa8892b044ece158cb4869b59f1972fa01aae6d2e"
 };
 
 export const getAllocationMarketplaceAddress = () => {
-    return "0x0c3C7552598ebbDA0e87564F66A80b3Fcbf3c358";
+    return "0x57AC70B020062dbe0A4F040e4371d125B8b25BC6";
 };
 
 export const getPancakeRouterAddress = () => {
-    return "0x9ac64cc6e4415144c455bd8e4837fea55603e5c3";
+    return "0x10ed43c718714eb63d5aa57b78b54704e256024e";
 };

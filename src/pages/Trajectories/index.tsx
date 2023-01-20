828c629b5fec3c7cad2ab0a098449745331239ad
danilitmo123 <danilitmo@gmail.com>
2023-01-20 17:04:10 +0200

    fixed

--- src/pages/Trajectories/index.tsx
+++ src/pages/Trajectories/index.tsx
@@ -1,17 +1,17 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, {useContext, useEffect, useState} from "react";
 import BgContext from "../../Context/Background";
 import BackButtonContext from "../../Context/BackButton";
-import { useNavigate, useSearchParams } from "react-router-dom";
+import {useNavigate, useSearchParams} from "react-router-dom";
 import axios from "axios";
 import * as Scroll from "react-scroll";
-import { CourseType, TrajectoryType } from "../../types";
+import {CourseType, TrajectoryType} from "../../types";
 import PercentProgress from "../../components/PercentProgress";
 import ControlTypeTile from "../../components/ControlTypeTile";
 import "./index.scss";
-import { allControllTypes, BASE_URL } from "../../constants";
+import {allControllTypes, BASE_URL} from "../../constants";
 import Close from "../../images/icons/close";
-import { LocalStorageInteraction, withLocalStorage } from "../../utils/general";
-import Chevron, { Turn } from "../../images/icons/chevron";
+import {LocalStorageInteraction, withLocalStorage} from "../../utils/general";
+import Chevron, {Turn} from "../../images/icons/chevron";
 import FeedbackGroupIdContext from "../../Context/IdGroup";
 import RandomFeedback from "../../components/Modals/feedback/randomFeedback";
 import {createStickyBlock, updateStickyBlocks} from "../../utils/stickyHeaders";
@@ -27,18 +27,19 @@ const randomFeedback = {
 
 
 const Trajectories = () => {
-  const { group_id } = useContext<any>(FeedbackGroupIdContext);
+  const {group_id} = useContext<any>(FeedbackGroupIdContext);
   const [width, setWidth] = useState(0);
   const [trajectories, setTrajectories] = useState([]);
   const [trajectoriesIds, setTrajectoriesIds] = useState([]);
-  const { setBg } = useContext(BgContext);
-  const { setNewBackButtonProps } = useContext(BackButtonContext);
-  const [searchParams, setSearchParams] = useSearchParams();
+  const {setBg} = useContext(BgContext);
+  const {setNewBackButtonProps} = useContext(BackButtonContext);
+  const [searchParams] = useSearchParams();
   const navigate = useNavigate();
+
   useEffect(() => {
     setBg("#F1F2F8");
 
-    const professionId = withLocalStorage({ professionId: null }, LocalStorageInteraction.load).professionId;
+    const professionId = withLocalStorage({professionId: null}, LocalStorageInteraction.load).professionId;
     setNewBackButtonProps("Выбор ключевых слов и пресетов", `/professionDetails?view=main&id=${professionId}`);
     if (trajectories.length === 0) {
       const trajectoryIds = JSON.parse(searchParams.get("ids") || "[]");
@@ -75,7 +76,7 @@ const Trajectories = () => {
     element.classList.remove("Hidden");
   };
   const trajectoryChosen = (trajectory: TrajectoryType, course = 1) => {
-    withLocalStorage({ chosenTrajectoriesIds: searchParams.get("ids") }, LocalStorageInteraction.save);
+    withLocalStorage({chosenTrajectoriesIds: searchParams.get("ids")}, LocalStorageInteraction.save);
     navigate(`/trajectory?id=${trajectory.id}&course=${course}`);
   };
   const scrollToRight = (event: any) => {
@@ -87,7 +88,7 @@ const Trajectories = () => {
     event.target.parentNode.scrollLeft -= Math.min(event.target.parentNode.clientWidth, 460);
   };
   const getControlTypesCount = (course: CourseType) => {
-    const controlTypes: { [key: string]: number } = { "exam": 0, "credit": 0, "diffCredit": 0, "coursework": 0 };
+    const controlTypes: { [key: string]: number } = {"exam": 0, "credit": 0, "diffCredit": 0, "coursework": 0};
     const nameToKey: { [key: string]: string } = {
       "Экзамен": "exam",
       "Зачет": "credit",
@@ -116,107 +117,109 @@ const Trajectories = () => {
             className="border-0 pr-0 py-0 hideButton"
             onClick={(e) => {
               const card = document.querySelector('.animationWrap')
-              if(card)
+              if (card)
                 card.classList.toggle('Hidden')
             }}
           >
-            <Close width={10} height={10} />
+            <Close width={10} height={10}/>
           </button>
         </div>
       </div>
 
-      {trajectories.map((trajectory: TrajectoryType) => {
-        return (
-          <div
-            className="TrajectoriesCard mb-3"
-            key={trajectory.id}
-          >
-            <div className="TrajectoriesCardHeader">
-              <h5 className="trajectoryHeader mb-0">
-                {trajectory.educational_plan}
-                <span className={"eduDirectionCode"}>{trajectory.code.replace(/\.$/, "")}</span>
-              </h5>
-              <div className="d-flex align-items-center TrajectoriesCardProgress">
-                <PercentProgress percent={trajectory.coverage} />
-                <span className="ml-2">
+      {
+        trajectories.map((trajectory: TrajectoryType) => {
+          return (
+            <div
+              className="TrajectoriesCard mb-3"
+              key={trajectory.id}
+            >
+              <div className="TrajectoriesCardHeader">
+                <h5 className="trajectoryHeader mb-0">
+                  {trajectory.educational_plan}
+                  <span className={"eduDirectionCode"}>{trajectory.code.replace(/\.$/, "")}</span>
+                </h5>
+                <div className="d-flex align-items-center TrajectoriesCardProgress">
+                  <PercentProgress percent={trajectory.coverage}/>
+                  <span className="ml-2">
                     {Math.round(trajectory.coverage * 100)}% совпадений
                   </span>
+                </div>
               </div>
-            </div>
-            <div style={{ position: "relative" }}>
-              <div className="mt-3 trajectoryCardWrapper HiddenLeft" onLoad={shouldDrawScrollButton}
-                   onScroll={shouldDrawScrollButton}>
-                <button className="ScrollBtn Right" onClick={scrollToRight}>
-                  <Chevron />
-                </button>
-                <button className="ScrollBtn Left" onClick={scrollToLeft}>
-                  <Chevron turn={Turn.left} />
-                </button>
-                {trajectory.courses.map((course, index) => {
-                  return (
-                    <div
-                      className={`CourseCard trajectories-bg-${index}`}
-                      key="index"
-                      onClick={() => trajectoryChosen(trajectory, index + 1)}
-                    >
-                      <div className="CourseCardHeader">{index + 1} курс</div>
-                      <div>
-                        <div className="mt-2 smallTitle">Ты изучишь</div>
-                        <div className="mt-2 keywordsWrapper row no-gutters">
-                          {course.main_keywords.slice(0, 5).map((keyword, index) => {
-                            return (
-                              <span
-                                key={keyword}
-                                className="keywordWrapper mr-2 mb-2"
-                              >
+              <div style={{position: "relative"}}>
+                <div className="mt-3 trajectoryCardWrapper HiddenLeft" onLoad={shouldDrawScrollButton}
+                     onScroll={shouldDrawScrollButton}>
+                  <button className="ScrollBtn Right" onClick={scrollToRight}>
+                    <Chevron/>
+                  </button>
+                  <button className="ScrollBtn Left" onClick={scrollToLeft}>
+                    <Chevron turn={Turn.left}/>
+                  </button>
+                  {trajectory.courses.map((course, index) => {
+                    return (
+                      <div
+                        className={`CourseCard trajectories-bg-${index}`}
+                        key="index"
+                        onClick={() => trajectoryChosen(trajectory, index + 1)}
+                      >
+                        <div className="CourseCardHeader">{index + 1} курс</div>
+                        <div>
+                          <div className="mt-2 smallTitle">Ты изучишь</div>
+                          <div className="mt-2 keywordsWrapper row no-gutters">
+                            {course.main_keywords.slice(0, 5).map((keyword, index) => {
+                              return (
+                                <span
+                                  key={keyword}
+                                  className="keywordWrapper mr-2 mb-2"
+                                >
                             {keyword}
                           </span>
-                            );
-                          })}
+                              );
+                            })}
+                          </div>
+                        </div>
+                        <div>
+                          <div className="mt-3 smallTitle">Ты сдашь</div>
+                          <div className="ControlCardContainer">
+                            {allControllTypes.map((controlTypeName) => {
+                              return (
+                                <ControlTypeTile
+                                  controlType={
+                                    course.control_types_count.find((controlType) => controlType.name === controlTypeName) ||
+                                    {name: controlTypeName, count: 0}
+                                  }
+                                />
+                              );
+                            })}
+                          </div>
                         </div>
                       </div>
-                      <div>
-                        <div className="mt-3 smallTitle">Ты сдашь</div>
-                        <div className="ControlCardContainer">
-                          {allControllTypes.map((controlTypeName) => {
-                            return (
-                              <ControlTypeTile
-                                controlType={
-                                  course.control_types_count.find((controlType) => controlType.name === controlTypeName) ||
-                                  { name: controlTypeName, count: 0 }
-                                }
-                              />
-                            );
-                          })}
-                        </div>
-                      </div>
-                    </div>
-                  );
-                })}
+                    );
+                  })}
+                </div>
+              </div>
+              <div className="mt-3 justify-content-between">
+                <div className="TrajectoriesCardFooter">
+                  <button
+                    onClick={() => trajectoryChosen(trajectory)}
+                    className="ButtonTrajectory MainButton mr-2"
+                  >
+                    Смотреть траекторию
+                  </button>
+                  <a
+                    href={`https://abit.itmo.ru/en/programs/bachelor?title=${trajectory?.educational_plan.replace("", "+")}`}
+                    target="_blank"
+                    className="ButtonAbit"
+                  >
+                    Читать больше на abit.itmo.ru
+                  </a>
+                </div>
               </div>
             </div>
-            <div className="mt-3 justify-content-between">
-              <div className="TrajectoriesCardFooter">
-                <button
-                  onClick={() => trajectoryChosen(trajectory)}
-                  className="ButtonTrajectory MainButton mr-2"
-                >
-                  Смотреть траекторию
-                </button>
-                <a
-                  href={`https://abit.itmo.ru/en/programs/bachelor?title=${trajectory?.educational_plan.replace("", "+")}`}
-                  target="_blank"
-                  className="ButtonAbit"
-                >
-                  Читать больше на abit.itmo.ru
-                </a>
-              </div>
-            </div>
-          </div>
-        );
-      })}
-      <RandomFeedback displayForGroup={2} />
-      <RandomFeedback displayForGroup={3} />
+          );
+        })
+      }
+      <RandomFeedback displayForGroup={2}/>
+      <RandomFeedback displayForGroup={3}/>
     </div>
   );
 };

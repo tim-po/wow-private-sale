48733a32b9a3ee0c410016dc651bd5dc105b009d
PetrMKO <74903849+PetrMKO@users.noreply.github.com>
2023-01-26 18:43:28 +0300

    Add background transition

--- src/components/AComponentSnippet/index.tsx
+++ src/components/AComponentSnippet/index.tsx
@@ -1,30 +1,19 @@
-import React, {useContext} from "react";
-import './index.scss'
+import React from "react";
+import "./index.scss";
 
 // CONSTANTS
 
 // DEFAULT FUNCTIONS
 
-// TODO: copy this components directory and add your content to make your page
-
-type SnippetComponentPropType = {
+interface ISnippetComponentProp {
     // You should declare props like this, delete this if you don't need props
-    someProp: any
-    somePropWithDefaultOption?: string
 }
 
-const SnippetComponentDefaultProps = {
-    // You should declare default props like this, delete this if you don't need props
-    somePropWithDefaultOption: 'default value'
-}
-
-const SnippetComponent = (props: SnippetComponentPropType) => {
+const SnippetComponent = (props: ISnippetComponentProp) => {
     return (
-        <div className={'some-classname'}>
-        </div>
-    )
+      <div className={"some-classname"}>
+      </div>
+    );
 };
 
-SnippetComponent.defaultProps = SnippetComponentDefaultProps
-
-export default SnippetComponent
+export default SnippetComponent;

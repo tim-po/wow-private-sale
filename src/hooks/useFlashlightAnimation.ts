427c26210f10a9f03c542513473abccfadb6ed52
PetrMKO <74903849+PetrMKO@users.noreply.github.com>
2023-01-29 18:02:22 +0300

    fix bug with bg

--- src/hooks/useFlashlightAnimation.ts
+++ src/hooks/useFlashlightAnimation.ts
@@ -6,10 +6,37 @@ export default (ref: RefObject<HTMLElement>, spotlightColor: Colors | string, wi
   let size = 0,
     maxSize = 100,
     at = { x: 0, y: 0 },
+    state: 'enter' | 'leave' = 'leave',
     enterTimer: NodeJS.Timer,
     leaveTimer: NodeJS.Timer,
     mainRender: NodeJS.Timer;
 
+
+  const clearBg = () =>{
+    if (ref.current && ref.current.style.background !== "") {
+      ref.current.style.background = "";
+    }
+  }
+
+  const setPosition = (event: MouseEvent<HTMLElement>) => {
+    if (ref.current) {
+      const element: HTMLElement = ref.current;
+      const { left, top } = element.getBoundingClientRect();
+      at = { x: event.clientX - left, y: event.clientY - top };
+    }
+  }
+
+  const renderFlashLight = () => {
+    if (ref.current) {
+      if(state === 'enter'){
+        const gradientCircle = `circle at ${at.x}px ${at.y}px`;
+        let colorTransition = `${spotlightColor} 0px, rgba(255, 255, 255, 0.01) ${size * 100 / maxSize}px`;
+        ref.current.style.background = `radial-gradient(${gradientCircle}, ${colorTransition}) no-repeat ${withBackgroundColor}`;
+      } else {
+        clearBg()
+      }
+    }
+  };
 
   const flashlightAppear = () => {
     if (ref.current) {
@@ -23,53 +50,31 @@ export default (ref: RefObject<HTMLElement>, spotlightColor: Colors | string, wi
   const flashlightDisappear = () => {
       size-=2.5
       if(size <= 0){
+        state='leave'
         clearInterval(mainRender)
         clearInterval(leaveTimer)
-        if(ref.current){
-          ref.current.style.background = '';
-        }
+        clearBg()
       }
   }
-
-  const renderFlashLight = () => {
-    if (ref.current) {
-      const gradientCircle = `circle at ${at.x}px ${at.y}px`;
-      let colorTransition = `${spotlightColor} 0px, rgba(255, 255, 255, 0.01) ${size * 100 / maxSize}px`;
-      ref.current.style.background = `radial-gradient(${gradientCircle}, ${colorTransition}) no-repeat ${withBackgroundColor}`;
-    }
-  };
-
+  
 
   return {
     enter: (event: MouseEvent<HTMLElement>) => {
+      state = 'enter'
       clearInterval(leaveTimer)
       mainRender = setInterval(renderFlashLight, 3)
-      enterTimer = setInterval(flashlightAppear, 3)
-
-      if (ref.current) {
-        const element: HTMLElement = ref.current;
-        const { left, top } = element.getBoundingClientRect();
-        at = { x: event.clientX - left, y: event.clientY - top };
-      }
+      enterTimer = setInterval(flashlightAppear, 1)
+      setPosition(event)
     },
 
     move: (event: MouseEvent<HTMLElement>) => {
-      if (ref.current) {
-        const element: HTMLElement = ref.current;
-        const { left, top } = element.getBoundingClientRect();
-        at = { x: event.clientX - left, y: event.clientY - top };
-      }
+      setPosition(event)
     },
 
     leave: (event: MouseEvent<HTMLElement>) => {
       clearInterval(enterTimer)
-      clearInterval(enterTimer)
-      leaveTimer = setInterval(flashlightDisappear, 3)
-      if (ref.current) {
-        const element: HTMLElement = ref.current;
-        const { left, top } = element.getBoundingClientRect();
-        at = { x: event.clientX - left, y: event.clientY - top };
-      }
+      leaveTimer = setInterval(flashlightDisappear, 1)
+      setPosition(event)
     }
   };
 }

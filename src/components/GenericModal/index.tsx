923033c28572a889279ec79074350b0db85405b5
TimPo <p.timon17@gmail.com>
2023-02-07 13:55:43 +0200

    fix naming

--- src/components/GenericModal/index.tsx
+++ src/components/GenericModal/index.tsx
@@ -1,30 +1,30 @@
-import React, { useEffect, useRef, useState } from "react";
-import "./index.scss";
-import Close from "../../images/icons/close";
-import { isMobile } from "react-device-detect";
+import React, { useEffect, useRef, useState } from 'react'
+import './index.scss'
+import Close from '../../images/icons/close'
+import { isMobile } from 'react-device-detect'
 
 // CONSTANTS
 
 // DEFAULT FUNCTIONS
 
 export type GenericModalOwnProps = {
-  hideMobile: boolean;
-  hideDesktop: boolean;
-  maxHeight?: boolean;
-  onModalClose: () => void;
-  colorCloseWhite: boolean;
-  children: React.ReactNode | React.ReactNode[];
-};
-
-export interface GenericModalFullProps extends GenericModalOwnProps {
-  isModalActive: boolean;
-  modalCount: number;
-  modalNumber: number;
-  refLastModals?: React.RefObject<HTMLDivElement> | undefined;
-  currentLastModals?: React.RefObject<HTMLDivElement> | undefined;
+  hideMobile: boolean
+  hideDesktop: boolean
+  maxHeight?: boolean
+  onModalClose: () => void
+  colorCloseWhite: boolean
+  children: React.ReactNode | React.ReactNode[]
 }
 
-export type OptionalGenericModalProps = Partial<GenericModalOwnProps>;
+export interface GenericModalFullProps extends GenericModalOwnProps {
+  isModalActive: boolean
+  modalCount: number
+  modalNumber: number
+  parentModalRef?: React.RefObject<HTMLDivElement> | undefined
+  currentLastModals?: React.RefObject<HTMLDivElement> | undefined
+}
+
+export type OptionalGenericModalProps = Partial<GenericModalOwnProps>
 
 const GenericModal = (props: GenericModalFullProps) => {
   const {
@@ -37,100 +37,96 @@ const GenericModal = (props: GenericModalFullProps) => {
     isModalActive,
     modalNumber,
     modalCount,
-    refLastModals,
+    parentModalRef,
     currentLastModals,
-  } = props;
+  } = props
 
-  const [isOpen, setIsOpen] = useState(false);
-  const [blockContent, setBlockContent] = useState(false);
-  const [height, setHeight] = useState<number>();
-  const swapElement = useRef<HTMLDivElement>(null);
-  const [cardHeight, setCardHeight] = useState<number>(0);
-  const [deltaY, setDeltaY] = useState(0);
-  const [prevMovementY, setPrevMovementY] = useState(0);
-  const [isTouched, setIsTouched] = useState(false);
+  const [isOpen, setIsOpen] = useState(false)
+  const [blockContent, setBlockContent] = useState(false)
+  const [height, setHeight] = useState<number>()
+  const swapElement = useRef<HTMLDivElement>(null)
+  const [cardHeight, setCardHeight] = useState<number>(0)
+  const [deltaY, setDeltaY] = useState(0)
+  const [prevMovementY, setPrevMovementY] = useState(0)
+  const [isTouched, setIsTouched] = useState(false)
 
   const touchStart = (e: any) => {
     if (!isTouched) {
       // @ts-ignore
-      setCardHeight(swapElement?.current?.offsetHeight);
-      setIsTouched(true);
-      setPrevMovementY(e.touches[0].clientY);
+      setCardHeight(swapElement?.current?.offsetHeight)
+      setIsTouched(true)
+      setPrevMovementY(e.touches[0].clientY)
     }
-  };
+  }
 
   const touchMove = (e: any) => {
     if (isTouched) {
-      const deltaY = prevMovementY - e.touches[0].clientY;
-      setDeltaY(deltaY);
+      const deltaY = prevMovementY - e.touches[0].clientY
+      setDeltaY(deltaY)
 
-      setCardHeight(cardHeight + deltaY);
+      setCardHeight(cardHeight + deltaY)
 
       if (deltaY >= 150) {
-        setCardHeight(window.screen.height - 80);
-        setIsTouched(false);
+        setCardHeight(window.screen.height - 80)
+        setIsTouched(false)
       }
 
       if (deltaY <= -150) {
-        setCardHeight(0);
-        setIsTouched(false);
-        modalClose();
+        setCardHeight(0)
+        setIsTouched(false)
+        modalClose()
       }
 
-      setPrevMovementY(e.touches[0].clientY);
+      setPrevMovementY(e.touches[0].clientY)
     }
-  };
+  }
 
   const touchEnd = () => {
     if (isTouched) {
-      setIsTouched(false);
-      setPrevMovementY(0);
+      setIsTouched(false)
+      setPrevMovementY(0)
 
       if (deltaY > 0) {
-        setCardHeight(250);
+        setCardHeight(250)
       }
 
       if (deltaY < 0) {
-        setCardHeight(0);
-        modalClose();
+        setCardHeight(0)
+        modalClose()
       }
     }
-  };
+  }
 
   const modalClose = () => {
-    setBlockContent(false);
+    setBlockContent(false)
     setTimeout(() => {
-      setIsOpen(false);
-    }, 400);
+      setIsOpen(false)
+    }, 400)
     setTimeout(() => {
-      onModalClose();
-    }, 500);
-  };
+      onModalClose()
+    }, 500)
+  }
 
   useEffect(() => {
-    setIsOpen(isModalActive);
-    setBlockContent(isModalActive);
-  }, [isModalActive]);
+    setIsOpen(isModalActive)
+    setBlockContent(isModalActive)
+  }, [isModalActive])
+
   useEffect(() => {
     const setHeightFunc = () => {
-      setHeight(currentLastModals?.current?.getBoundingClientRect().top);
-    };
+      setHeight(currentLastModals?.current?.getBoundingClientRect().top)
+    }
 
     if (currentLastModals?.current) {
-      currentLastModals.current.addEventListener(
-        "animationstart",
-        setHeightFunc
-      );
+      currentLastModals.current.addEventListener('animationstart', setHeightFunc)
     }
     return () => {
       if (currentLastModals?.current) {
-        currentLastModals.current.removeEventListener(
-          "animationstart",
-          setHeightFunc
-        );
+        currentLastModals.current.removeEventListener('animationstart', setHeightFunc)
       }
-    };
-  });
+    }
+  }, [currentLastModals?.current])
+
   const isAnimationModal = () => {
     if (isMobile && blockContent) {
       if (modalNumber === 0) {
@@ -141,43 +137,30 @@ const GenericModal = (props: GenericModalFullProps) => {
             }
           : {
               minHeight: `${cardHeight}px`,
-            };
+            }
       }
       return {
         minHeight: `${cardHeight}px`,
-      };
+      }
     }
-    return {};
-  };
+    return {}
+  }
   return (
     <div
-      className={`
-      ModalContainer 
-      ${hideMobile ? "hideMobile" : ""} 
-      ${hideDesktop ? "hideDesktop" : ""}
-      ${isOpen ? "active" : ""}
-      ${maxHeight ? "maxHeight" : ""}
-      `}
+      className={`ModalContainer ${hideMobile ? 'hideMobile' : ''} ${
+        hideDesktop ? 'hideDesktop' : ''
+      } ${isOpen ? 'active' : ''} ${maxHeight ? 'maxHeight' : ''}`}
     >
-      {isOpen && (
-        <div className="ModalContainerShad deck" onClick={modalClose} />
-      )}
+      {isOpen && <div className="ModalContainerShad deck" onClick={modalClose} />}
       <div className="ModalTrack">
-        {isOpen && (
-          <div className="ModalContainerShad mobil" onClick={modalClose} />
-        )}
+        {isOpen && <div className="ModalContainerShad mobil" onClick={modalClose} />}
         <div
-          ref={modalCount > 1 ? refLastModals : null}
-          className={`
-          d-block
-          TextCenter 
-          ${blockContent ? "active" : ""}  
-         
-          `}
+          ref={modalCount > 1 ? parentModalRef : null}
+          className={`d-block TextCenter ${blockContent ? 'active' : ''}`}
           style={isAnimationModal()}
         >
           <div
-            className={"wrapHeaderModal"}
+            className={'wrapHeaderModal'}
             onTouchStart={touchStart}
             onTouchMove={touchMove}
             onTouchEnd={touchEnd}
@@ -186,7 +169,7 @@ const GenericModal = (props: GenericModalFullProps) => {
               <Close
                 width={12}
                 height={12}
-                color={colorCloseWhite ? "white" : undefined}
+                color={colorCloseWhite ? 'white' : undefined}
               />
             </button>
           </div>
@@ -197,7 +180,7 @@ const GenericModal = (props: GenericModalFullProps) => {
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default GenericModal;
+export default GenericModal
--- src/generic/layout/index.tsx
+++ src/generic/layout/index.tsx
@@ -1,76 +1,66 @@
-import React, {
-  ReactNode,
-  useContext,
-  useEffect,
-  useRef,
-  useState,
-} from "react";
-import "./index.scss";
-import GenericModal, {
-  OptionalGenericModalProps,
-} from "components/GenericModal";
-import Header from "components/Header";
-import BackButtonContext from "Context/BackButton";
-import ModalsContext from "Context/Modal";
-import { useCookies } from "react-cookie";
-import axios from "axios";
-import { BASE_URL } from "../../constants";
-import FeedbackGroupIdContext from "../../Context/IdGroup";
-import { updateStickyBlocks } from "../../utils/stickyHeaders";
+import React, { ReactNode, useContext, useEffect, useRef, useState } from 'react'
+import './index.scss'
+import GenericModal, { OptionalGenericModalProps } from 'components/GenericModal'
+import Header from 'components/Header'
+import BackButtonContext from 'Context/BackButton'
+import ModalsContext from 'Context/Modal'
+import { useCookies } from 'react-cookie'
+import axios from 'axios'
+import { BASE_URL } from '../../constants'
+import FeedbackGroupIdContext from '../../Context/IdGroup'
+import { updateStickyBlocks } from '../../utils/stickyHeaders'
 // CONSTANTS
 
 // DEFAULT FUNCTIONS
 
 type layoutPropType = {
   // You should declare props like this, delete this if you don't need props
-  children: React.ReactElement | React.ReactElement[];
-};
+  children: React.ReactElement | React.ReactElement[]
+}
 
 const Layout = (props: layoutPropType) => {
-  const { children } = props;
+  const { children } = props
 
-  const [shouldDisplayModal, setShouldDisplayModal] = useState<boolean>(false);
+  const [shouldDisplayModal, setShouldDisplayModal] = useState<boolean>(false)
   const [modalComponent, setModalComponent] = useState<
     Array<{
-      component: ReactNode | undefined;
-      props?: OptionalGenericModalProps;
+      component: ReactNode | undefined
+      props?: OptionalGenericModalProps
     }>
-  >([]);
+  >([])
 
-  const { backButtonHref } = useContext(BackButtonContext);
-  const [cookie] = useCookies(["_ym_uid"]);
+  const { backButtonHref } = useContext(BackButtonContext)
+  const [cookie] = useCookies(['_ym_uid'])
 
-  const [groupId, setGroupId] = useState<number>(0);
-  const refLastModals = useRef<HTMLDivElement>(null);
+  const [groupId, setGroupId] = useState<number>(0)
+  const refLastModals = useRef<HTMLDivElement>(null)
 
   const displayModal = (
     component: React.ReactNode,
-    genericProps?: OptionalGenericModalProps
+    genericProps?: OptionalGenericModalProps,
   ) => {
-    setModalComponent((prevState) => [
+    setModalComponent(prevState => [
       ...prevState,
       {
         component: component,
         props: genericProps,
       },
-    ]);
-    setShouldDisplayModal(true);
-  };
+    ])
+    setShouldDisplayModal(true)
+  }
 
   const closeModal = () => {
-    setModalComponent(modalComponent.slice(0, -1));
+    setModalComponent(modalComponent.slice(0, -1))
     // setShouldDisplayModal(false);
-  };
+  }
 
   useEffect(() => {
-    axios
-      .get(`${BASE_URL}feedback/split_group?group_id=${cookie._ym_uid}`)
-      .then((res) => {
-        setGroupId(res.data.group_id);
-      });
+    axios.get(`${BASE_URL}feedback/split_group?group_id=${cookie._ym_uid}`).then(res => {
+      setGroupId(res.data.group_id)
+    })
 
-    updateStickyBlocks();
-  }, []);
+    updateStickyBlocks()
+  }, [])
 
   // useEffect(() => {
   //   if (shouldDisplayModal) {
@@ -82,26 +72,24 @@ const Layout = (props: layoutPropType) => {
 
   useEffect(() => {
     if (modalComponent.length > 0) {
-      document.body.style.overflow = "hidden";
+      document.body.style.overflow = 'hidden'
     } else {
-      document.body.style.overflow = "unset";
+      document.body.style.overflow = 'unset'
     }
-  }, [modalComponent]);
+  }, [modalComponent])
 
   return (
     <ModalsContext.Provider value={{ displayModal, closeModal }}>
       <FeedbackGroupIdContext.Provider value={{ groupId }}>
-        {/*<BgContext.Provider value={{setBg: setBackgroundColor}}>*/}
+        {/* <BgContext.Provider value={{setBg: setBackgroundColor}}>*/}
         <div className="DefaultLayoutContainer" id="scroll-container">
-          <Header left={backButtonHref === "/"} />
+          <Header left={backButtonHref === '/'} />
           <div className="Content">
             {children}
             {modalComponent.map((item, index) => (
               <GenericModal
-                refLastModals={
-                  modalComponent.length - 1 === index
-                    ? refLastModals
-                    : undefined
+                parentModalRef={
+                  modalComponent.length - 1 === index ? refLastModals : undefined
                 }
                 modalCount={modalComponent.length}
                 currentLastModals={refLastModals}
@@ -118,10 +106,10 @@ const Layout = (props: layoutPropType) => {
             ))}
           </div>
         </div>
-        {/*</BgContext.Provider>*/}
+        {/* </BgContext.Provider>*/}
       </FeedbackGroupIdContext.Provider>
     </ModalsContext.Provider>
-  );
-};
+  )
+}
 
-export default Layout;
+export default Layout

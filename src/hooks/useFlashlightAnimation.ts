8144c8799fcc14e530284ae473a4cce45f1db402
PetrMKO <74903849+PetrMKO@users.noreply.github.com>
2023-02-12 22:52:16 +0300

    Animation have a side effect

--- src/hooks/useFlashlightAnimation.ts
+++ src/hooks/useFlashlightAnimation.ts
@@ -1,80 +1,107 @@
-import { MouseEvent, RefObject } from "react";
-import { Colors } from "../utils/background";
+import { MouseEvent, RefObject, useEffect } from 'react'
+import { Colors } from '../utils/background'
 
-export default (ref: RefObject<HTMLElement>, spotlightColor: Colors | string, withBackgroundColor: string, isMobile:boolean) => {
-
+export default (
+  ref: RefObject<HTMLElement>,
+  spotlightColor: Colors | string,
+  withBackgroundColor: string,
+  isMobile: boolean,
+) => {
+  const maxSize = 100
   let size = 0,
-    maxSize = 100,
     at = { x: 0, y: 0 },
-    state: 'enter' | 'leave' = 'leave',
+    state: 'enter' | 'leave' | 'rerender' = 'leave',
     enterTimer: NodeJS.Timer,
     leaveTimer: NodeJS.Timer,
-    mainRender: NodeJS.Timer;
+    mainRender: NodeJS.Timer
 
-
-  const clearBg = () =>{
-    if (ref.current && ref.current.style.background !== "") {
-      ref.current.style.background = "";
+  const clearBg = () => {
+    if (ref.current && ref.current.style.background !== '') {
+      ref.current.style.background = ''
     }
   }
 
   const setPosition = (event: MouseEvent<HTMLElement>) => {
     if (ref.current) {
-      const element: HTMLElement = ref.current;
-      const { left, top } = element.getBoundingClientRect();
-      at = { x: event.clientX - left, y: event.clientY - top };
+      const element: HTMLElement = ref.current
+      const { left, top } = element.getBoundingClientRect()
+      at = { x: event.clientX - left, y: event.clientY - top }
     }
   }
 
   const renderFlashLight = () => {
     if (ref.current) {
-      if(state === 'enter'){
-        const gradientCircle = `circle at ${at.x}px ${at.y}px`;
-        let colorTransition = `${spotlightColor} 0px, rgba(255, 255, 255, 0.01) ${size * 100 / maxSize}px`;
-        ref.current.style.background = `radial-gradient(${gradientCircle}, ${colorTransition}) no-repeat ${withBackgroundColor}`;
+      if (state === 'enter') {
+        const gradientCircle = `circle at ${at.x}px ${at.y}px`
+        const colorTransition = `${spotlightColor} 0px, rgba(255, 255, 255, 0.01) ${
+          (size * 100) / maxSize
+        }px`
+        ref.current.style.background = `radial-gradient(${gradientCircle}, ${colorTransition}) no-repeat ${withBackgroundColor}`
       } else {
         clearBg()
       }
     }
-  };
+  }
 
   const flashlightAppear = () => {
     if (ref.current) {
-      size+=2.5
-      if(size >= maxSize){
+      size += 2.5
+      if (size >= maxSize) {
         clearInterval(enterTimer)
       }
     }
   }
 
   const flashlightDisappear = () => {
-      size-=2.5
-      if(size <= 0){
-        state='leave'
-        clearInterval(mainRender)
-        clearInterval(leaveTimer)
-        clearBg()
-      }
-  }
-
-
-  return isMobile ? undefined : {
-    onMouseEnter: (event: MouseEvent<HTMLElement>) => {
-      state = 'enter'
+    size -= 2.5
+    if (size <= 0) {
+      state = 'leave'
+      // clearInterval(mainRender)
       clearInterval(leaveTimer)
-      mainRender = setInterval(renderFlashLight, 3)
-      enterTimer = setInterval(flashlightAppear, 1)
-      setPosition(event)
-    },
-
-    onMouseMove: (event: MouseEvent<HTMLElement>) => {
-      setPosition(event)
-    },
-
-    onMouseLeave: (event: MouseEvent<HTMLElement>) => {
       clearInterval(enterTimer)
-      leaveTimer = setInterval(flashlightDisappear, 1)
-      setPosition(event)
+      clearBg()
     }
   }
+
+  useEffect(() => {
+    // isMount.current = setInterval(renderFlashLight, 3)
+
+    mainRender = setInterval(renderFlashLight, 3)
+    console.log('enter')
+
+    return () => {
+      // isMount.current = false
+      clearInterval(mainRender)
+      // state = 'rerender'
+      console.log('unmount')
+    }
+  })
+
+  return isMobile
+    ? { animation: {} }
+    : {
+        animation: {
+          onMouseEnter: (event: MouseEvent<HTMLElement>) => {
+            clearInterval(mainRender)
+            mainRender = setInterval(renderFlashLight, 3)
+            console.log('enter')
+            state = 'enter'
+            clearInterval(leaveTimer)
+            clearInterval(enterTimer)
+            enterTimer = setInterval(flashlightAppear, 1)
+
+            setPosition(event)
+          },
+          onMouseMove: (event: MouseEvent<HTMLElement>) => {
+            setPosition(event)
+          },
+          onMouseLeave: (event: MouseEvent<HTMLElement>) => {
+            console.log('leave')
+            clearInterval(leaveTimer)
+            clearInterval(enterTimer)
+            leaveTimer = setInterval(flashlightDisappear, 1)
+            setPosition(event)
+          },
+        },
+      }
 }

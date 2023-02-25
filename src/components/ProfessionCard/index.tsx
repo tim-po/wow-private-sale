a2cda0d538a50e966839c5ffaf25f2eec7fa3eb3
TimPo <p.timon17@gmail.com>
2023-02-25 12:15:55 +0300

    fix width

--- src/components/ProfessionCard/index.tsx
+++ src/components/ProfessionCard/index.tsx
@@ -1,6 +1,6 @@
-import React, {useContext, useEffect} from "react";
-import {COLORS_BY_CATEGORY} from "../../constants";
-import {Profession} from "../../types";
+import React, { useContext, useEffect } from 'react'
+import { COLORS_BY_CATEGORY } from '../../constants'
+import { Profession } from '../../types'
 import './index.scss'
 
 // CONSTANTS
@@ -12,12 +12,12 @@ type ProfessionCardPropType = {
 }
 
 const ProfessionCard = (props: ProfessionCardPropType) => {
-  const {profession} = props;
+  const { profession } = props
 
   const animateSvg = (svg: Element) => {
     let style: HTMLElement | null = document.getElementById('svgAnimeStyles')
     if (!style) {
-      style = document.createElement('style');
+      style = document.createElement('style')
       style.setAttribute('type', 'text/css')
       style.setAttribute('id', 'svgAnimeStyles')
     }
@@ -35,10 +35,10 @@ const ProfessionCard = (props: ProfessionCardPropType) => {
       const css = `.ProfessionCardButton:hover .icon-wrapper-${profession.id} .n${index}{${cssTransform}}`
 
       if (style) {
-        style.appendChild(document.createTextNode(css));
+        style.appendChild(document.createTextNode(css))
       }
     })
-    document.getElementsByTagName('head')[0].appendChild(style);
+    document.getElementsByTagName('head')[0].appendChild(style)
   }
 
   useEffect(() => {
@@ -46,15 +46,6 @@ const ProfessionCard = (props: ProfessionCardPropType) => {
       const div = document.getElementsByClassName(`icon-wrapper-${profession.id}`)[0]
       div.innerHTML = profession.svg
       animateSvg(div)
-      // div.innerHTML = this.profession.svg
-      // this.animateSvg(div)
-      // fetch(`/api/professions/${this.profession.id}/svg/`)
-      //   .then(res => res.json())
-      //   .then(res => {
-      //     const div = document.getElementsByClassName(`icon-wrapper-${this.profession.id}`)[0]
-      //     div.innerHTML = this.profession.svg
-      //     this.animateSvg(div)
-      //   })
     }
   })
 
@@ -63,25 +54,27 @@ const ProfessionCard = (props: ProfessionCardPropType) => {
       <div className="cardData">
         <h4 className="professionCardTitle">{profession.name}</h4>
         <div className="professionCardKeywords">
-          {profession.visible_keywords.sort((word1, word2) => {
-            return (word1.text.length - word2.text.length)
-          }).map((keyword, index) => {
-            return (
-              <div
-                key={keyword.id}
-                className="professionCardKeyword" style={{background: COLORS_BY_CATEGORY[profession.category]}}
-              >
-                {keyword.text}
-              </div>
-            )
-          })}
+          {profession.visible_keywords
+            .sort((word1, word2) => {
+              return word1.text.length - word2.text.length
+            })
+            .map((keyword, index) => {
+              return (
+                <div
+                  key={keyword.id}
+                  className="professionCardKeyword"
+                  style={{ background: COLORS_BY_CATEGORY[profession.category] }}
+                >
+                  {keyword.text}
+                </div>
+              )
+            })}
         </div>
         <div className="professionCardCategory">{profession.category}</div>
       </div>
-      <div className={`icon-wrapper-${profession.id}`}/>
+      <div className={`icon-wrapper-${profession.id}`} />
     </div>
   )
-};
+}
 
 export default ProfessionCard
-

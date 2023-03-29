6370307224795871748736f98dd3b987d9d6b943
danilitmo123 <danilitmo@gmail.com>
2023-03-29 12:54:56 +0300

    add mmpro logo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,30 +2,54 @@ import React from 'react';
 import styled from "styled-components";
 import WOWLogo from '../../icons/wow'
 import ConnectWalletButton from "../ConnectWalletButton";
+import MMProLogo from "../../images/MMProLogo";
 
 const Wrapper = styled.div`
-	position: relative;
-	display: flex;
-	align-items: center;
-	justify-content: space-between;
-	padding: 15px 60px 20px;
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 15px 60px 20px;
 
-	@media screen and (max-width: 1000px) {
-		padding: 23px;
-	}
+  @media screen and (max-width: 1000px) {
+    padding: 23px;
+  }
 `
 
-const Logo = styled.img`
-  width: 62px;
-  height: 52px;
+const LogosWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  gap: 12px;
+
+  @media screen and (max-width: 750px) {
+    flex-direction: column;
+    align-items: flex-start;
+    width: 50%;
+    gap: 0;
+  }
+`
+
+const LogoWrapper = styled.div`
+	svg {
+    @media screen and (max-width: 450px) {
+      width: 180px;
+    }
+	}
 `
 
 const Header = () => {
 	return (
 		<Wrapper>
-
-			<WOWLogo />
-			<ConnectWalletButton />
+			<LogosWrapper>
+				<LogoWrapper>
+					<WOWLogo/>
+				</LogoWrapper>
+				<LogoWrapper>
+					<MMProLogo/>
+				</LogoWrapper>
+			</LogosWrapper>
+			<ConnectWalletButton/>
 		</Wrapper>
 	);
 };
